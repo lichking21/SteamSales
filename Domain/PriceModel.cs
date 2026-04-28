8e6119e0bf69877e5fd264efc2c21a974bbc223d
lichking21 <kazakovaman@gmail.com>
2026-04-28 22:53:06 +0200

    (new) price info response structure

---
+++ Domain/PriceModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain;
+
+public class PriceOverview
+{
+    [JsonPropertyName("discount_percent")]
+    public int DiscountPercent {get; set;}
+
+    [JsonPropertyName("initial_formatted")]
+    public string InitialPrice {get;set;} = "N/A";
+
+    [JsonPropertyName("final_formatted")]
+    public string FinalPrice {get;set;} = "N/A";
+}
+
+public class GameData
+{
+    [JsonPropertyName("price_overview")]
+    public PriceOverview? Price {get;set;}
+
+    [JsonPropertyName("is_free")]
+    public bool IsFree {get;set;}
+}
+
+public class GameDetails
+{
+    [JsonPropertyName("success")]
+    public bool Success {get;set;}
+
+    [JsonPropertyName("data")]
+    public GameData? Data {get;set;}
+}
